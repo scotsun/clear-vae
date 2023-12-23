8bc7e329b7964ee53d96cb7ea6b2d6b6a3dd9abd
Scott Sun <minghuisunscott@gmail.com>
2023-12-23 18:44:13 -0500

    finished kl_est simulation

--- mgvae/model.py
+++ mgvae/model.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.distributions as dis
+from tqdm import tqdm
+
+from torch.utils.data import DataLoader
 
 
 def log_mg_prior(z, mu_major, logvar_major):
@@ -54,19 +57,40 @@ def kl_lb_loss(mu_minor, logvar_minor, mu_major, logvar_major):
     return kl.mean()
 
 
-def mgvae_loss(x_reconstr, x, mu_minor, logvar_minor, mu_major, logvar_major):
-    reconstruction_loss = F.binary_cross_entropy(x_reconstr, x, reduction="mean")
-    kl_regularization = kl_lb_loss(mu_minor, logvar_minor, mu_major, logvar_major)
-    return reconstruction_loss + kl_regularization
+def mgvae_loss(
+    x_reconstr, x, mu_minor, logvar_minor, mu_major, logvar_major, kl_loss_fn: str
+):
+    n_dims = len(x.shape)
+    reconstruction_loss = (
+        F.binary_cross_entropy(x_reconstr, x, reduction="none")
+        .sum(dim=list(range(n_dims))[1:])
+        .mean()
+    )
+    match kl_loss_fn:
+        case "estimated":
+            kl_regularization = kl_estimated_loss(
+                mu_minor, logvar_minor, mu_major, logvar_major
+            )
+        case "lower_bound":
+            kl_regularization = kl_lb_loss(
+                mu_minor, logvar_minor, mu_major, logvar_major
+            )
+        case _:
+            raise ValueError("incorrect 'kl_loss_fn' value.")
+    return reconstruction_loss, kl_regularization
 
 
 class MGVAE(nn.Module):
-    def __init__(self, input_dim, hidden_dim, latent_dim) -> None:
+    def __init__(
+        self, input_dim, hidden_dim, latent_dim, majority_dataloader: DataLoader
+    ) -> None:
         super().__init__()
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
+        self.majority_dataloader = majority_dataloader
 
-        self.majority_prior_logvar = nn.Parameter(torch.zeros(1))
+        self.logvar_major = nn.Parameter(torch.zeros(1))
 
         self.encoder = nn.Sequential(
             nn.Linear(input_dim, hidden_dim),
@@ -108,6 +132,55 @@ class MGVAE(nn.Module):
         z = self.sample(mu, logvar)
         x_hat = self.decode(z)
         return x_hat, mu, logvar
+
+    def compute_mg_prior_mu(self):
+        N = len(self.majority_dataloader.dataset)
+        mu_major = torch.zeros(N, self.latent_dim)
+        idx_cursor = 0
+        with torch.no_grad():
+            for X_batch, _ in self.majority_dataloader:
+                mu_batch = self.encode(X_batch)
+                mu_major[idx_cursor : (idx_cursor + len(X_batch)), :] = mu_batch
+                idx_cursor += len(X_batch)
+        return mu_major
+
+    def evaluate(self, dataloader):
+        # TODO:
+        pass
+
+    def train(self, train_loader, kl_loss_fn, lr, epochs, verbose_period):
+        optimizer = torch.optim.Adam(self.parameters(), lr=lr)
+        for epoch in range(epochs):
+            total_recon_loss, total_kl_loss = 0.0, 0.0
+            total_batch = 0.0
+            verbose = (epoch % verbose_period) == 0
+            with tqdm(
+                train_loader, unit="batch", miniterval=0, disable=not verbose
+            ) as bar:
+                bar.set_description(f"Epoch {epoch}")
+                for X_batch, _ in bar:
+                    optimizer.zero_grad()
+                    mu_major = self.compute_mg_prior_mu()
+                    Xh_batch, mu_batch, logvar_batch = self(X_batch)
+                    recon_loss, kl_loss = mgvae_loss(
+                        Xh_batch,
+                        X_batch,
+                        mu_batch,
+                        logvar_batch,
+                        mu_major,
+                        self.logvar_major,
+                        kl_loss_fn,
+                    )
+                    (recon_loss + kl_loss).backward()
+                    optimizer.step()
+                    # update progress
+                    total_recon_loss += recon_loss.item()
+                    total_kl_loss += kl_loss.item()
+                    total_batch += 1
+                    bar.set_postfix(
+                        recon_loss=float(total_recon_loss / total_batch),
+                        kl_loss=float(total_kl_loss / total_batch),
+                    )
 
 
 # if __name__ == "__main__":
