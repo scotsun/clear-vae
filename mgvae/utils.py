eeb538c8e1be94785332ef327342ff17528f7096
Scott Sun <minghuisunscott@gmail.com>
2024-02-07 23:13:19 -0500

    Update utils.py

--- mgvae/utils.py
+++ mgvae/utils.py
@@ -33,3 +33,36 @@ def generate_imbalanced_dataset(
     imbalanced_dataset = ConcatDataset([majority, minority])
     print(f"majority size: {len(majority)}, minority size: {len(minority)}")
     return imbalanced_dataset
+
+
+class Corrputed_Dataset(Dataset):
+    def __init__(self, data, label, corruption_fn, transform_fn):
+        """
+        label is corresponding to the corruption type
+        """
+        self.data = data
+        N = len(self.data)
+        self.label = label * torch.ones(N)
+        self.corruption_fn = corruption_fn
+        self.transform_fn = transform_fn
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        x = self.transform_fn(self.corruption_fn(self.data[idx]))
+        y = self.label[idx]
+        return x, y
+
+
+def generate_imbalanced_corrupted_dataset(
+    orig_data_list, corruption_style_list, transform_fn
+) -> Dataset:
+    """Create an imbalanced dataset with different corruption approaches."""
+    imbalanced_dataset_list = []
+    for i, corrupt_fn in enumerate(corruption_style_list):
+        data_c = Corrputed_Dataset(orig_data_list[i], i, corrupt_fn, transform_fn)
+        imbalanced_dataset_list.append(data_c)
+    imbalanced_dataset = ConcatDataset(imbalanced_dataset_list)
+    print([len(elem) for elem in orig_data_list])
+    return imbalanced_dataset
