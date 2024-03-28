2368d3b04e97e24f3bc63171d034f67402e66187
Scott Sun <minghuisunscott@gmail.com>
2024-03-28 15:31:13 -0400

    Update utils.py

--- contrastive-vae/src/utils.py
+++ contrastive-vae/src/utils.py
@@ -1,55 +1,58 @@
 """Data utility functions."""
 
-import random
+import numpy as np
+import torch
+import torchvision
 from torch.utils.data import Dataset
 from tqdm import tqdm
 from IPython import display
 import torchvision.transforms as transforms
 
 
-class ImbalancePairGenerator:
-    """
-    Object that make pairs randomly from a imbalanced dataset.
-    """
-
+class MNISTPairGenerator:
     def __init__(
-        self,
-        majority_data: list,
-        minority_data: list,
-        corruption_fn_choices: None | list = None,
+        self, dataset: torchvision.datasets.MNIST, p: float, corruption_fns: None | dict
     ) -> None:
-        self.majority_data = majority_data
-        self.minority_data = minority_data
-        self.corruption_fn_choices = corruption_fn_choices
+        label_dict = dict()
+        for i in range(10):
+            label_dict[i] = torch.where(dataset.train_labels == i)[0]
+        self.dataset = dataset
+        self.corruption_fns = corruption_fns
+        self.p = p
+        self.label_dict = label_dict
 
-    def _sample_img(self, from_majority: bool):
-        """helper function to sample a pairing img."""
-        if from_majority:
-            img, _ = random.choice(self.majority_data)
-        else:
-            img, _ = random.choice(self.minority_data)
-        return img
+    def __getitem__(self, index):
+        base_img, target = self.dataset[index]
+        pair_content_label = np.random.binomial(1, self.p)
+        if pair_content_label == 0:  # matched
+            pair_img_idx = np.random.choice(self.label_dict[target], size=1)[0]
+        else:  # unmatched
+            unmatched_idx = torch.where(self.dataset.train_labels != target)[0]
+            pair_img_idx = np.random.choice(unmatched_idx, size=1)[0]
 
-    def __getitem__(self, idx):
-        if idx < len(self.majority_data):
-            base_img, _ = self.majority_data[idx]
-            pair_img = self._sample_img(from_majority=True)
-            label = 0.0
-        elif idx >= len(self.majority_data) and idx < len(self.majority_data) + len(
-            self.minority_data
-        ):
-            base_img, _ = self.minority_data[idx - len(self.majority_data)]
-            pair_img = self._sample_img(from_majority=True)
-            label = 1.0
+        if self.corruption_fns is not None:
+            base_cfn = np.random.choice(
+                list(self.corruption_fns.keys()), p=list(self.corruption_fns.values())
+            )
+            pair_cfn = np.random.choice(
+                list(self.corruption_fns.keys()), p=list(self.corruption_fns.values())
+            )
+
+            pair_style_label = 0 if base_cfn == pair_cfn else 1
         else:
-            raise IndexError("Index out of range")
-        # add corruption
-        if self.corruption_fn_choices:
-            corruption_fn_base = random.choice(self.corruption_fn_choices)
-            corruption_fn_pair = random.choice(self.corruption_fn_choices)
-            base_img = corruption_fn_base(base_img)
-            pair_img = corruption_fn_pair(pair_img)
-        return base_img, pair_img, label
+            pair_style_label = 0
+
+        pair_img, _ = self.dataset[pair_img_idx]
+        return (
+            base_cfn(base_img),
+            pair_cfn(pair_img),
+            pair_content_label,
+            pair_style_label,
+        )
+
+    @property
+    def size(self):
+        return len(self.dataset)
 
 
 class PairDataset(Dataset):
@@ -57,10 +60,10 @@ class PairDataset(Dataset):
     Dataset object based on a pair generator.
     """
 
-    def __init__(self, generator: ImbalancePairGenerator, transform):
+    def __init__(self, generator, transform):
         self.generator = generator
         self.transform = transform
-        self.N = len(generator.majority_data) + len(generator.minority_data)
+        self.N = generator.size
         self.dataset = [None] * self.N
         with tqdm(range(self.N), unit="item") as bar:
             bar.set_description("Generating dataset")
@@ -71,11 +74,11 @@ class PairDataset(Dataset):
         return self.N
 
     def __getitem__(self, idx):
-        img1, img2, label = self.dataset[idx]
+        img1, img2, content_label, style_label = self.dataset[idx]
         img1, img2 = self.transform(img1), self.transform(img2)
-        return img1, img2, label
+        return img1, img2, content_label, style_label
 
     def display(self, idx):
-        img1, img2, pair_label = self.__getitem__(idx)
-        print(f"label: {int(pair_label)}")
+        img1, img2, content_label, style_label = self.__getitem__(idx)
+        print(f"content label: {int(content_label)}, style label: {style_label}")
         display(transforms.ToPILImage()(img1), transforms.ToPILImage()(img2))
