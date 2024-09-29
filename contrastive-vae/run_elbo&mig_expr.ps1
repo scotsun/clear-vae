fbbf9cdd39b08464fa21cd50e53b21a349357373
Scott Sun <minghuisunscott@gmail.com>
2024-09-29 17:25:38 -0400

    Create run_elbo&mig_expr.ps1

---
+++ contrastive-vae/run_elbo&mig_expr.ps1
@@ -0,0 +1,4 @@
+# run elbo & mig, compare ml-vae & clear-vae
+python run_elbo_mig_expr.py --temperature 0.1
+python run_elbo_mig_expr.py --temperature 0.3
+python run_elbo_mig_expr.py --temperature 1.0
